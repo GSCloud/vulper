d701a6a4e0c4ecf5695f6c4882d34b81ed01de9e
Filip Oščádal / 1. Web IT <oscadal@1webit.cz>
2022-05-04 09:25:10 +0200

    updated .env parser

--- bin/envparser.php
+++ bin/envparser.php
@@ -1,30 +1,31 @@
 <?php
-declare(strict_types=1);
+declare (strict_types = 1);
 
-// pass file to be parsed as first argument
+// pass the file resource to be parsed as the first and only parameter
 if ($argc == 2) {
     $f = $argv[1];
     if (file_exists($f) && is_readable($f)) {
         $arr = file($f);
-        $arr = array_map('trim', $arr);
         $arr = array_map(function ($s) {
-            preg_match('/([a-zA-Z0-9_@$%^&* \-\.,:\/="\']+)/', $s, $matches);
-            if ($matches && strpos($matches[1], '=') !== false) {
+            preg_match('/([a-zA-Z0-9_@$%^&* \-\.,:\/="\']+)/', $s, $matches, PREG_UNMATCHED_AS_NULL);
+            if ($matches && array_key_exists(1, $matches) && strpos($matches[1], '=') !== false) {
                 return $matches[1];
             }
         }, $arr);
         $arr = array_map('trim', $arr);
         $arr = array_filter($arr);
+        sort($arr);
+
         $res = [];
-        foreach($arr as $s) {
-            $x = explode('=', $s);
-            $y = trim($x[1], '"\'');
-            if (is_numeric($y)) {
-                $y = (int) $y;
+        foreach ($arr as $s) {
+            $x = explode('=', $s, 2);
+            $key = $x[0];
+            $val = trim($x[1], '"\'');
+            if (is_numeric($val)) {
+                $val = (int) $val;
             }
-            $res[$x[0]] = $y;
+            $res[$key] = $val;
         }
-        //echo json_encode($res);
         echo json_encode($res, JSON_PRETTY_PRINT);
     }
 }
